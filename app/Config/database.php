b376e22c13cdea6623dcf29b65dafe459a30953d
yuta yamamoto <tuki0918@gmail.com>
2015-06-13 17:07:23 +0900

    add eloquent set up file

---
+++ app/Config/database.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Capsule\Manager as DB;
+
+$db = new DB;
+
+$db->addConnection([
+    'driver'    => 'mysql',
+    'host'      => '****',
+    'database'  => '****',
+    'username'  => '****',
+    'password'  => '****',
+    'charset'   => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix'    => '',
+]);
+
+$db->setAsGlobal();
+
+$db->bootEloquent();
