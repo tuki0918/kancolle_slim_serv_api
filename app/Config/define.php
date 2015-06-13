978b7e1c0ae392c99b00b6f18192958c76a35262
yuta yamamoto <tuki0918@gmail.com>
2015-06-14 01:18:45 +0900

    config: define directory path

--- app/Config/define.php
+++ app/Config/define.php
@@ -1,6 +1,16 @@
 <?php
 
+// Database Config
 define('DB_HOST', '****');
 define('DB_NAME', '****');
 define('DB_USER', '****');
 define('DB_PASS', '****');
+
+// Directory Path
+define('BASE_PATH'     , realpath(__DIR__ . '/../..'));
+define('APP_PATH'      , BASE_PATH . '/app');
+define('CONFIG_PATH'   , APP_PATH  . '/Config');
+define('ROUTE_PATH'    , APP_PATH  . '/Route');
+define('VIEW_PATH'     , APP_PATH  . '/View');
+define('TEMPLATE_PATH' , VIEW_PATH . '/Template');
+define('PUBLIC_PATH'   , '/public');
