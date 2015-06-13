8eedca1fe1d6b749b7e786430968399d3e128f69
yuta yamamoto <tuki0918@gmail.com>
2015-06-13 22:06:47 +0900

    composer psr-4 app directory add

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInita029d7d51d502ba893ee64b2bbafc652::getLoader();
+return ComposerAutoloaderInitc6ebaf8ec011bcd1d04e1c1085ac7314::getLoader();
