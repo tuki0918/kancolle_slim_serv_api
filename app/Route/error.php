1fbff4d25cf11ada505a55db055f693fd8a416bc
yuta yamamoto <tuki0918@gmail.com>
2015-06-14 11:57:07 +0900

    add routing error handling

--- app/Route/error.php
+++ app/Route/error.php
@@ -1,1 +1,9 @@
 <?php
+
+$app->notFound(function () use ($app) {
+    $app->render('error/404.php');
+});
+
+$app->error(function (\Exception $e) use ($app) {
+    $app->render('error/500.php');
+});
