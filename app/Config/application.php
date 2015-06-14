cdfeefee4725f31c78ce4b0151292fa76a1109db
yuta yamamoto <tuki0918@gmail.com>
2015-06-14 17:32:12 +0900

    monolog import slim application

--- app/Config/application.php
+++ app/Config/application.php
@@ -13,3 +13,10 @@ $app->configureMode('production', function () use ($app) {
 $app->configureMode('development', function () use ($app) {
     $app->config('debug', true);
 });
+
+// Logging setup
+$app->container->singleton('log', function () {
+    $log = new Monolog\Logger(APP_NAME);
+    $log->pushHandler(new Monolog\Handler\StreamHandler(LOG_FILENAME, Monolog\Logger::DEBUG));
+    return $log;
+});
