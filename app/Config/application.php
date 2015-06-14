67c742c4d1eb4bc7d8226b8a2cbe26c9fa90114c
yuta yamamoto <tuki0918@gmail.com>
2015-06-15 00:41:34 +0900

    refactoring: twig setup

--- app/Config/application.php
+++ app/Config/application.php
@@ -4,6 +4,7 @@
 $app->config('mode', APP_MODE);
 $app->config('templates.path', VIEW_PATH);
 
+
 // if mode is "production"
 $app->configureMode('production', function () use ($app) {
     $app->config('debug', false);
@@ -14,15 +15,19 @@ $app->configureMode('development', function () use ($app) {
     $app->config('debug', true);
 });
 
+
 // View template
-$view = $app->view();
-$view->parserOptions = array(
+$app->config('view', new Slim\Views\Twig());
+
+$app->view->parserOptions = array(
     'debug' => true,
     'cache' => VIEW_PATH . '/cache'
 );
-$view->parserExtensions = array(
+
+$app->view->parserExtensions = array(
     new Slim\Views\TwigExtension()
 );
+
 
 // Logging setup
 $app->container->singleton('log', function () {
--- public/index.php
+++ public/index.php
@@ -2,9 +2,7 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$app = new Slim\Slim(array(
-    'view' => new Slim\Views\Twig()
-));
+$app = new Slim\Slim();
 
 // application setup
 require_once CONFIG_PATH . '/application.php';
