5bcbdb8fe2e95b1af29a5ba36d2bbbffc463a647
yuta yamamoto <tuki0918@gmail.com>
2015-06-14 17:39:12 +0900

    add monolog call in the controller

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -5,9 +5,11 @@ namespace App\Controller;
 class AppController
 {
     protected $app;
+    protected $log;
 
     public function __construct($app)
     {
         $this->app = $app;
+        $this->log = $app->log;
     }
 }
