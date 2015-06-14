026f116adb4e5c6b859d3ff2a3828d4fc723cf96
yuta yamamoto <tuki0918@gmail.com>
2015-06-14 17:43:15 +0900

    define log settings

--- app/Config/define.php
+++ app/Config/define.php
@@ -13,7 +13,11 @@ define('APP_MODE'      , 'production');
 // Directory Path
 define('BASE_PATH'     , realpath(__DIR__ . '/../..'));
 define('APP_PATH'      , BASE_PATH . '/app');
+define('LOGS_PATH'     , BASE_PATH . '/logs');
 define('CONFIG_PATH'   , APP_PATH  . '/Config');
 define('ROUTE_PATH'    , APP_PATH  . '/Route');
 define('VIEW_PATH'     , APP_PATH  . '/View');
 define('PUBLIC_PATH'   , '/public');
+
+// File Path
+define('LOG_FILENAME'  , LOGS_PATH . '/application.log');
