1a239b411585d34f480f969dbdac9743ec5d3011
yuta yamamoto <tuki0918@gmail.com>
2015-06-14 11:56:13 +0900

    add User Model

---
+++ app/Model/User.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model as Eloquent;
+
+class User extends Eloquent {
+
+    protected $table = 'users';
+
+    protected $hidden = array('password');
+}
