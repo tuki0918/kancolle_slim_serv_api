b1d6debab67fde8ee6f2982309dc1f7c8911018f
yuta yamamoto <tuki0918@gmail.com>
2015-06-21 19:49:37 +0900

    modify: database config export outside file

--- app/Config/eloquent.php
+++ app/Config/eloquent.php
@@ -2,18 +2,11 @@
 
 use Illuminate\Database\Capsule\Manager as DB;
 
+$config = require CONFIG_PATH . '/database.php';
+
 $db = new DB;
 
-$db->addConnection([
-    'driver'    => 'mysql',
-    'host'      => DB_HOST,
-    'database'  => DB_NAME,
-    'username'  => DB_USER,
-    'password'  => DB_PASS,
-    'charset'   => 'utf8',
-    'collation' => 'utf8_unicode_ci',
-    'prefix'    => '',
-]);
+$db->addConnection($config);
 
 $db->setAsGlobal();
 
