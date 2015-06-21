ea4bf61ae6caa49528e7446df7c422607b5679b4
yuta yamamoto <tuki0918@gmail.com>
2015-06-21 10:13:58 +0900

    add: slim command line run code

---
+++ command/run.php
@@ -0,0 +1,25 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = new Slim\Slim();
+
+// convert all the command line arguments into a URL
+$argv = $GLOBALS['argv'];
+array_shift($GLOBALS['argv']);
+$pathInfo = '/' . implode('/', $argv);
+
+// Set up the environment so that Slim can route
+$app->environment = Slim\Environment::mock([
+    'PATH_INFO' => $pathInfo
+]);
+
+// application setup
+require_once CONFIG_PATH . '/application.php';
+
+// application routing setup
+foreach (glob(ROUTE_PATH . '/cli.*.php') as $inc) {
+    require_once $inc;
+}
+
+$app->run();
