1f4518fb31afe8ec810ff5fb2e32e217e5881158
yuta yamamoto <tuki0918@gmail.com>
2015-06-21 23:50:01 +0900

    refactoring: phpmig in pimple run code

--- phpmig.php
+++ phpmig.php
@@ -2,18 +2,23 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
-$config = require CONFIG_PATH . '/database.php';
-
-$db = new Illuminate\Database\Capsule\Manager;
-$db->addConnection($config);
-$db->setAsGlobal();
-$db->bootEloquent();
+use Illuminate\Database\Capsule\Manager as Capsule;
 
 $container = new Pimple();
 
-$container['phpmig.adapter'] = function() use ($db) {
-    return new Phpmig\Adapter\Illuminate\Database($db, 'migrations');
-};
+$container['db.config'] = require CONFIG_PATH . '/database.php';
+
+$container['db'] = $container->share(function($c) {
+    $db = new Capsule;
+    $db->addConnection($c['db.config']);
+    $db->setAsGlobal();
+    $db->bootEloquent();
+    return $db;
+});
+
+$container['phpmig.adapter'] = $container->share(function($c) {
+    return new Phpmig\Adapter\Illuminate\Database($c['db'], 'migrations');
+});
 
 $container['phpmig.migrations_path'] = __DIR__ . DIRECTORY_SEPARATOR . 'migrations';
 
