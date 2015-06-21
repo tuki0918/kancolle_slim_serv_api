907d4073adf659061c52e44c63b0b44424e24290
yuta yamamoto <tuki0918@gmail.com>
2015-06-21 19:51:26 +0900

    refactoring: eloquent setup file

--- app/Config/eloquent.php
+++ app/Config/eloquent.php
@@ -5,9 +5,6 @@ use Illuminate\Database\Capsule\Manager as DB;
 $config = require CONFIG_PATH . '/database.php';
 
 $db = new DB;
-
 $db->addConnection($config);
-
 $db->setAsGlobal();
-
 $db->bootEloquent();
