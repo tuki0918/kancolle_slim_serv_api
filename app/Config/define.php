86d271d317e1283d3b9453dc51d4de44d8cbf397
yuta yamamoto <tuki0918@gmail.com>
2015-06-27 10:54:26 +0900

    add: cookie setup

--- app/Config/define.php
+++ app/Config/define.php
@@ -9,6 +9,9 @@ define('DB_PASS', '****');
 // Application Infomation
 define('APP_NAME'      , 'slim');
 define('APP_MODE'      , 'production');
+define('APP_COOKIE_NAME'      , 'slim_session');
+define('APP_COOKIE_SECRET_KEY', 'CHANGE_ME');
+define('APP_COOKIE_EXPIRES'   , '20 minutes');
 
 // Directory Path
 define('BASE_PATH'     , realpath(__DIR__ . '/../..'));
--- public/index.php
+++ public/index.php
@@ -6,6 +6,18 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $app = new Slim\Slim();
 
+$app->add(new Slim\Middleware\SessionCookie(array(
+    'expires' => '20 minutes',
+    'path' => '/',
+    'domain' => null,
+    'secure' => false,
+    'httponly' => false,
+    'name' => 'slim_session',
+    'secret' => 'CHANGE_ME',
+    'cipher' => MCRYPT_RIJNDAEL_256,
+    'cipher_mode' => MCRYPT_MODE_CBC
+)));
+
 // application setup
 require_once CONFIG_PATH . '/application.php';
 
