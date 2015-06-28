58eb3c09c918ca223cbcd9119ca8ec18b7636b57
yuta yamamoto <tuki0918@gmail.com>
2015-06-28 18:30:44 +0900

    add: capistrano tmp directory changed premission

--- private/capistrano/config/deploy.rb
+++ private/capistrano/config/deploy.rb
@@ -14,10 +14,13 @@ set :repo_url, "git@github.com:tuki0918/#{fetch :application}.git"
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push('app/Config/define.php')
+set :linked_files, %w{app/Config/define.php}
 
 # Default value for linked_dirs is []
-set :linked_dirs, fetch(:linked_dirs, []).push('logs')
+set :linked_dirs, %w{app/tmp/logs}
+
+# Default value for tmp_dirs is []
+set :tmp_dirs, %w{app/tmp app/tmp/logs app/tmp/cache}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
@@ -27,4 +30,22 @@ set :keep_releases, 10
 
 namespace :deploy do
 
+  desc "Fix permission in :tmp_dirs."
+  task :fix_premission do
+    on roles(:app) do
+      dirs = fetch(:tmp_dirs)
+      if dirs.is_a?(Array)
+        dirs.each do |d|
+          path = current_path.join(d)
+          if test "[ ! -d #{path} ]"
+            execute "mkdir -p #{path}"
+          end
+          execute "chmod 0777 #{path}"
+        end
+      end
+    end
+  end
+
 end
+
+after 'deploy:finished', 'deploy:fix_premission'
